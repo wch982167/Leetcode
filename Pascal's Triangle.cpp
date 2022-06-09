5c49a33d00581555938e3e86048c5946ac55434d
wch982167 <76194616+wch982167@users.noreply.github.com>
2022-06-09 22:17:30 +0800

    Create Pascal's Triangle.cpp

---
+++ Pascal's Triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res(numRows);
+        for(int i=0;i<numRows;i++){
+            vector<int> tmp(i+1);
+            res[i] = tmp;
+            res[i][0] = 1;
+            res[i][i] = 1;
+            for(int j=1;j<i;j++){
+                res[i][j] = res[i-1][j-1]+res[i-1][j];
+            }
+        }
+        return res;
+    }
+};
